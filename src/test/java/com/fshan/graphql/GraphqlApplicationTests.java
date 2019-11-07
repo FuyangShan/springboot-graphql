ac3dccff32a60476f99f2e861c35e4e293cb3a54
Shan Fuyang <fuyang@Shans-MBP.attlocal.net>
2019-11-07 13:25:01 -0800

    Starting Code

---
+++ src/test/java/com/fshan/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fshan.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
